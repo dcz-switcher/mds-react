8bdf526a18391926b473cf9b198521f72c4179f1
David CHOLLEZ <dchollez@macif.fr>
2024-11-25 15:04:37 +0100

    enhance story

--- src/components/CardCompactV/CardCompactV.stories.tsx
+++ src/components/CardCompactV/CardCompactV.stories.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import CardCompactV from './CardCompactV';
 
 const meta = {
   component: CardCompactV,
+  decorators: [
+    (Story) => (
+      <div className='mds-row'>
+        <div className="mds-col-12 mds-col-sm-4 mds-col-md-2">
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof CardCompactV>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {args: {
+  title: "Véhicules et mobilité"
+}};
