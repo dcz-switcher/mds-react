aa89cb4b264ff6b0cac1c36a55c4ef3d851538c8
David CHOLLEZ <dchollez@macif.fr>
2025-07-14 20:56:58 +0200

    Add dot variant

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -14,6 +14,7 @@ export const Default: Story = {
   args: {
     label: "My badge",
     severity: "new",
-    size: "small"
+    size: "small",
+    variant: "default"
   }
 };
--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,32 +2,49 @@ import React from 'react';
 
 
 export type BadgeProps = {
+    ariaLabel?: string;
     label?: string;
     size?: "small" | "big";
     severity?: "neutral" | "success" | "warning" | "danger" | "new";
     color?: string; // override severity color
     icon?: string;
+    variant?: "default" | "dot";
     className?: string;
 }
 
 
 export default function Badge ({
+    ariaLabel,
     label,
     size = "small",
     severity = "neutral",
     icon,
     color,
+    variant = "default",
     className = '',
 }: BadgeProps) {
 
     const colorClass = (color) ? `mds-background-color__${color}` : "";
 
     return  (
-        <p className={`mds-badge mds-badge--${size} mds-badge--${severity} ${colorClass} ${className}`}>
-            {icon &&
-                <span className={`mds-icon__${icon}`} aria-hidden="true"></span>
-            }
-            <span>{label}</span>
-        </p>
+        variant === "default" ?
+            <p className={`mds-badge mds-badge--${size} mds-badge--${severity} ${colorClass} ${className}`}>
+                {icon &&
+                    <span className={`mds-icon__${icon}`} aria-hidden="true"></span>
+                }
+                <span>{label}</span>
+            </p>
+        : 
+            <p className={`mds-badge mds-badge--dot mds-badge--${size} mds-badge--${severity} ${colorClass} ${className}`}>
+                { (icon && size === "big") ? 
+                    <span className={`mds-icon__${icon}`} aria-hidden="true"></span>
+                : 
+                    size === "big" ? 
+                        label
+                    :
+                        ""
+                }
+                <span className="mds-sr-only">{ariaLabel}</span>
+            </p>
     )
 }
